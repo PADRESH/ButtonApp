e70e9b01ec0849ffa44a8cbea86503964d5e1186
Vic <s>
2020-11-27 19:09:32 +0300

    First

---
+++ ButtonApp/ButtonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonAppApp.swift
+//  ButtonApp
+//
+//  Created by Victor Shadrin on 27.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct ButtonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
